9baf57e89c08b6e6112cd4258e6a01e6adfa5cbb
chu.yingjie <tochuyingjie@126.com>
2021-03-31 18:00:53 +0800

    init project

---
+++ nuxt.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    router: {
+        //自定义路由规则
+        extendRoutes(routes, resolve) {
+            // console.log('routes',routes)
+            routes.splice(0)
+
+            routes.push(...[
+                {
+                    path: '/',
+                    name: 'layout',
+                    component: resolve(__dirname, 'pages/layout/'),
+                    children: [
+                        {
+                            path: '',
+                            name: 'home',
+                            component: resolve(__dirname, 'pages/home/')
+                        },
+                        {
+                            path: 'pages/about',
+                            name: 'about',
+                            component: resolve(__dirname, 'pages/about/')
+                        },
+                        {
+                            path: '/login',
+                            name: 'login',
+                            component: resolve(__dirname, 'pages/login/')
+                        },
+                        {
+                            path: '/register',
+                            name: 'register',
+                            component: resolve(__dirname, 'pages/login/')
+                        }
+                    ]
+                },
+
+            ])
+        }
+    }
+}
